bdf1d87b8df459ee843cfb33b3c9ff3316147e93
Olivier Lebel <olleb25@ulaval.ca>
2023-11-25 09:13:35 -0500

    Ajout exceptions

--- bourse.py
+++ bourse.py
@@ -2,24 +2,30 @@ import json
 import argparse
 from datetime import timedelta, date
 import requests
+from exceptions import ErreurDate
 
 
 class Bourse : 
     def prix(self, symbole, d):
+        if d.year - date.today().year > 0 or d.month - date.today().month > 0 or d.day - date.today().day > 0:
+            raise ErreurDate()
+        
+        if d.year - date.today().year == 0 and d.month - date.today().month == 0 and d.day - date.today().day == 0:
+            hier = timedelta(days = 2)
+            d -= hier
+
+        if d.year - date.today().year == 0 and d.month - date.today().month == 0 and d.day - date.today().day == -1:
+            hier = timedelta(days = 1)
+            d -= hier
+
         if d.weekday() == 5:
             vendredi = timedelta(days = 1)
             d -= vendredi
-        elif d.weekday() == 6:
+
+        if d.weekday() == 6:
             vendredi = timedelta(days= 2)
             d -= vendredi 
-        elif d.year - date.today().year == 0 and d.month - date.today().month == 0 and d.day - date.today().day == 0:
-            hier = timedelta(days = 2)
-            d -= hier
-        elif d.year - date.today().year == 0 and d.month - date.today().month == 0 and d.day - date.today().day == -1:
-            hier = timedelta(days = 1)
-            d -= hier
-        elif d.year - date.today().year < 0 or d.month - date.today().month < 0 or d.day - date.today().day < -1:
-            print('Erreur')
+        
 
         url = f'https://pax.ulaval.ca/action/{symbole}/historique/'
         params = {'début' : d, 'fin' : d}
@@ -29,5 +35,5 @@ class Bourse :
 
 
 bourse = Bourse()
-d = date(2023, 11, 24)
+d = date(2023, 11, 26)
 print(bourse.prix('AAPL', d))
---
+++ exceptions.py
@@ -0,0 +1,13 @@
+class ErreurDate(RuntimeError):
+    def __str__(self):
+        return "Date invalide!"
+    
+
+class ErreurQuantité(RuntimeError):
+    def __str__(self):
+        return "Quantité invalide!"
+    
+
+class LiquiditéInsuffisante(RuntimeError):
+    def __str__(self):
+        return "Liquidité insuffisante!"
