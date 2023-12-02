74a70b5a6650fbe587cf0d4fad59172e31af19f9
Olivier Lebel <olleb25@ulaval.ca>
2023-12-02 09:27:49 -0500

    final 4

--- bourse.py
+++ bourse.py
@@ -1,31 +1,26 @@
 import json
-import argparse
 from datetime import timedelta, date
 import requests
 from exceptions import ErreurDate
 
 
-class Bourse : 
+class Bourse :
     def prix(self, symbole, d):
         auj = date.today()
         if d.year - auj.year > 0 or d.month - auj.month > 0 or d.day - auj.day > 0:
             raise ErreurDate()
-        
         if d.year - auj.year == 0 and d.month - auj.month == 0 and d.day - auj.day == 0:
             hier = timedelta(days = 3)
             d -= hier
-
         if d.weekday() == 5:
             vendredi = timedelta(days = 1)
             d -= vendredi
-
         if d.weekday() == 6:
             vendredi = timedelta(days= 2)
             d -= vendredi 
-        
-
         url = f'https://pax.ulaval.ca/action/{symbole}/historique/'
         params = {'début' : d, 'fin' : d}
         reponse = requests.get(url=url, params=params)
         reponse = json.loads(reponse.text)
         return reponse['historique'][d.strftime('%Y-%m-%d')]['fermeture']
+    
--- exceptions.py
+++ exceptions.py
@@ -1,13 +1,12 @@
 class ErreurDate(RuntimeError):
     def __str__(self):
         return "Date invalide!"
-    
 
 class ErreurQuantité(RuntimeError):
     def __str__(self):
         return "Quantité invalide!"
     
-
 class LiquiditéInsuffisante(RuntimeError):
     def __str__(self):
         return "Liquidité insuffisante!"
+    
