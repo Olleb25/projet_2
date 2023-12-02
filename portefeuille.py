c1dcb8afcf683405c955f6b31e02500d884fb3a7
Olivier Lebel <olleb25@ulaval.ca>
2023-12-02 09:24:28 -0500

    final 3

--- portefeuille.py
+++ portefeuille.py
@@ -8,12 +8,11 @@ class Portefeuille():
             d = date.today()
         elif d.year - date.today().year > 0 or d.month - date.today().month > 0 or d.day - date.today().day > 0:
             raise ErreurDate()
-        
         self.bourse = Bourse()
-        self.I = {'date' : {f'{d}' : {f'montant_{d}' : montant, f'actions_{d}' : {'A' : 0, 'AAPL' : 0, 'C' : 0, 
-            'GOOG' : 0, 'HOG' : 0, 'HPQ' : 0, 'INTC' : 0, 'IBM' : 0, 'LUV' : 0, 'MMM' : 0, 'MSFT' : 0, 
+        self.I = {'date' : {f'{d}' : {f'montant_{d}' : montant, f'actions_{d}' : {'A' : 0, 'AAPL' : 0, 'C' : 0,
+            'GOOG' : 0, 'HOG' : 0, 'HPQ' : 0, 'INTC' : 0, 'IBM' : 0, 'LUV' : 0, 'MMM' : 0, 'MSFT' : 0,
                 'T' : 0, 'TGT' : 0, 'TXN' : 0, 'XOM' : 0, 'WMT' : 0}}}}
-        
+    
     def __interval(self, d2):
         d1 = datetime.strptime(list(self.I['date'])[-1], '%Y-%m-%d').date()
         x = self.I['date'][f'{d1}']
@@ -59,11 +58,11 @@ class Portefeuille():
         if montant_total > montant_portefeuille:
             raise LiquiditéInsuffisante()
         else:
-            self.I['date'][f'{d}'] = {f'montant_{d}' : x[list(x)[0]] - montant_total, f'actions_{d}' : {'A' : x[list(x)[1]]['A'], 
-                'AAPL' : x[list(x)[1]]['AAPL'], 'C' : x[list(x)[1]]['C'], 'GOOG' : x[list(x)[1]]['GOOG'], 'HOG' : x[list(x)[1]]['HOG'], 
-                    'HPQ' : x[list(x)[1]]['HPQ'], 'INTC' : x[list(x)[1]]['INTC'], 'IBM' : x[list(x)[1]]['IBM'], 
-                        'LUV' : x[list(x)[1]]['LUV'], 'MMM' : x[list(x)[1]]['MMM'], 'MSFT' : x[list(x)[1]]['MSFT'], 
-                            'T' : x[list(x)[1]]['T'], 'TGT' : x[list(x)[1]]['TGT'], 'TXN' : x[list(x)[1]]['TXN'], 
+            self.I['date'][f'{d}'] = {f'montant_{d}' : x[list(x)[0]] - montant_total, f'actions_{d}' : {'A' : x[list(x)[1]]['A'],
+                'AAPL' : x[list(x)[1]]['AAPL'], 'C' : x[list(x)[1]]['C'], 'GOOG' : x[list(x)[1]]['GOOG'], 'HOG' : x[list(x)[1]]['HOG'],
+                    'HPQ' : x[list(x)[1]]['HPQ'], 'INTC' : x[list(x)[1]]['INTC'], 'IBM' : x[list(x)[1]]['IBM'],
+                        'LUV' : x[list(x)[1]]['LUV'], 'MMM' : x[list(x)[1]]['MMM'], 'MSFT' : x[list(x)[1]]['MSFT'],
+                            'T' : x[list(x)[1]]['T'], 'TGT' : x[list(x)[1]]['TGT'], 'TXN' : x[list(x)[1]]['TXN'],
                                 'XOM' : x[list(x)[1]]['XOM'], 'WMT' : x[list(x)[1]]['WMT']}}
 
             self.I['date'][f'{d}'][f'actions_{d}'][symbole] += quantité
@@ -111,7 +110,7 @@ class Portefeuille():
             d = date.today()
         elif d.year - date.today().year > 0 or d.month - date.today().month > 0 or d.day - date.today().day > 0:
             raise ErreurDate()
-        
+
         self.__interval(d)
         valeur_totale = 0
         nombre = self.titres(d)
