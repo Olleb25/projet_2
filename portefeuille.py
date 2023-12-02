9afd0751ef81abfa2469b4cd5a181168fbda53e6
Olivier Lebel <olleb25@ulaval.ca>
2023-12-02 09:48:14 -0500

    final 7

--- portefeuille.py
+++ portefeuille.py
@@ -9,7 +9,7 @@ class Portefeuille():
         elif d.year - date.today().year > 0 or d.month - date.today().month > 0 or d.day - date.today().day > 0:
             raise ErreurDate()
         self.bourse = Bourse()
-        self.I = {'date' : {f'{d}' : {f'montant_{d}' : montant, f'actions_{d}' : 
+        self.I = {'date' : {f'{d}' : {f'montant_{d}' : montant, f'actions_{d}' :
             {'A' : 0, 'AAPL' : 0, 'C' : 0, 'GOOG' : 0, 'HOG' : 0, 'HPQ' : 0,
                 'INTC' : 0, 'IBM' : 0, 'LUV' : 0, 'MMM' : 0, 'MSFT' : 0,
                     'T' : 0, 'TGT' : 0, 'TXN' : 0, 'XOM' : 0, 'WMT' : 0}}}}
@@ -32,7 +32,8 @@ class Portefeuille():
         self.__interval(d)
 
         x = self.I['date'][list(self.I['date'])[-1]]
-        self.I['date'][f'{d}'] = {f'montant_{d}' : x[list(x)[0]] + montant, f'actions_{d}' : x[list(x)[1]]}
+        self.I['date'][f'{d}'] = {f'montant_{d}' : x[list(x)[0]] + montant,
+            f'actions_{d}' : x[list(x)[1]]}
 
         x = self.I['date'][list(self.I['date'])[-1]][f'montant_{d}']
 
@@ -59,12 +60,13 @@ class Portefeuille():
         if montant_total > montant_portefeuille:
             raise LiquiditéInsuffisante()
         else:
-            self.I['date'][f'{d}'] = {f'montant_{d}' : x[list(x)[0]] - montant_total, f'actions_{d}' : {'A' : x[list(x)[1]]['A'],
-                'AAPL' : x[list(x)[1]]['AAPL'], 'C' : x[list(x)[1]]['C'], 'GOOG' : x[list(x)[1]]['GOOG'], 'HOG' : x[list(x)[1]]['HOG'],
-                    'HPQ' : x[list(x)[1]]['HPQ'], 'INTC' : x[list(x)[1]]['INTC'], 'IBM' : x[list(x)[1]]['IBM'],
-                        'LUV' : x[list(x)[1]]['LUV'], 'MMM' : x[list(x)[1]]['MMM'], 'MSFT' : x[list(x)[1]]['MSFT'],
-                            'T' : x[list(x)[1]]['T'], 'TGT' : x[list(x)[1]]['TGT'], 'TXN' : x[list(x)[1]]['TXN'],
-                                'XOM' : x[list(x)[1]]['XOM'], 'WMT' : x[list(x)[1]]['WMT']}}
+            self.I['date'][f'{d}'] = {f'montant_{d}' : x[list(x)[0]] - montant_total, f'actions_{d}' :
+                {'A' : x[list(x)[1]]['A'], 'AAPL' : x[list(x)[1]]['AAPL'],
+                    'C' : x[list(x)[1]]['C'], 'GOOG' : x[list(x)[1]]['GOOG'], 'HOG' : x[list(x)[1]]['HOG'],
+                        'HPQ' : x[list(x)[1]]['HPQ'], 'INTC' : x[list(x)[1]]['INTC'], 'IBM' : x[list(x)[1]]['IBM'],
+                            'LUV' : x[list(x)[1]]['LUV'], 'MMM' : x[list(x)[1]]['MMM'], 'MSFT' : x[list(x)[1]]['MSFT'],
+                                'T' : x[list(x)[1]]['T'], 'TGT' : x[list(x)[1]]['TGT'], 'TXN' : x[list(x)[1]]['TXN'],
+                                    'XOM' : x[list(x)[1]]['XOM'], 'WMT' : x[list(x)[1]]['WMT']}}
 
             self.I['date'][f'{d}'][f'actions_{d}'][symbole] += quantité
 
@@ -142,7 +144,7 @@ class Portefeuille():
     def valeur_projetée(self, rendement, d = None):
         if d is None or d == date.today():
             return "Aucun rendement à prévoir!"
-        elif d - date.today() < timedelta(days=0):
+        if d - date.today() < timedelta(days=0):
             raise ErreurDate()
 
         de = list(self.I['date'])[-1]
